9853118f6721303ed6e3e1e152509816faa06df1
Svitovan (PC) <svitovan@outlook.com>
2025-09-03 21:22:35 +0200

    upd

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,7 +10,7 @@ function Card() {
     <div className="card">
       <Title />
       <Count count={count}/>
-      <ResetButton />
+      <ResetButton setCount={setCount}/>
       <CountButtons setCount = {setCount} />
     </div>
   );
--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,8 +1,9 @@
 import {ResetIcon } from '@radix-ui/react-icons';
 
-function resetButton() {
+function resetButton({setCount}) {
+  
   return (
-     <button className="reset-btn">
+     <button onClick={()=> {setCount(0)}} className="reset-btn">
       <ResetIcon className='reset-btn-icon'/>
      </button>
   );
